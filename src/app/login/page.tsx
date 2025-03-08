13261e2007f101208e80fcfb736d420c2d04f5cb
Zeinerdev <zeinerdev@example.com>
2025-03-08 23:58:55 +0800

    Set login page as default

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+
+export default function LoginPage() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <p>Welcome to the Chicago Bulls Blog. Please log in.</p>
+      </div>
+    );
+  }
+  
